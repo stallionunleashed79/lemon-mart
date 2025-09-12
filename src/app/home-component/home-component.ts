1a7ab64c28ef0598c694103ebebc83e718459074
Bharath Seshadri <bharath.seshadri@photon.com>
2025-09-12 16:57:43 +0530

    Created the feature modules for the application

---
+++ src/app/home-component/home-component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-component',
+  imports: [],
+  template: `
+    <p>
+      home-component works!
+    </p>
+  `,
+  styles: ``
+})
+export class HomeComponent {
+
+}
---
+++ src/app/page-not-found-component/page-not-found-component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-not-found-component',
+  imports: [],
+  template: `
+    <p>
+      page-not-found-component works!
+    </p>
+  `,
+  styles: ``
+})
+export class PageNotFoundComponent {
+
+}
