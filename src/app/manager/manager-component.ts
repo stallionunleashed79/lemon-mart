4e28e24af7ae28a4d48c9d0a1dd26a59313c0b92
Bharath Seshadri <bharath.seshadri@photon.com>
2025-09-13 15:47:53 +0530

    Implemented buttons for POS and Inventory modules

--- src/app/manager/manager-component.ts
+++ src/app/manager/manager-component.ts
@@ -3,14 +3,31 @@ import { MatToolbar } from '@angular/material/toolbar';
 import { InventoryRoutingModule } from '../inventory/inventory-routing-module';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-manager-component',
-  imports: [MatToolbar, InventoryRoutingModule, MatButtonModule, RouterLink, RouterOutlet],
+  imports: [
+    MatToolbar,
+    InventoryRoutingModule,
+    MatButtonModule,
+    RouterLink,
+    MatIconModule,
+    RouterOutlet,
+    MatTooltipModule,
+  ],
   template: `<mat-toolbar color="accent" class="tool-bar">
       <a mat-button routerLink="home" routerLinkActive="active-link"> Manager's Dashboard </a>
       <a mat-button routerLink="users" routerLinkActive="active-link"> User Management </a>
       <a mat-button routerLink="receipts" routerLinkActive="active-link"> Receipt Lookup </a>
+      <span class="flex-spacer"></span>
+      <button mat-mini-fab routerLink="/inventory" matTooltip="Inventory" aria-label="Inventory">
+        <mat-icon>list</mat-icon>
+      </button>
+      <button mat-mini-fab routerLink="/pos" matTooltip="POS" aria- label="POS">
+        <mat-icon>shopping_cart</mat-icon>
+      </button>
     </mat-toolbar>
     <router-outlet></router-outlet> `,
   styles: `
