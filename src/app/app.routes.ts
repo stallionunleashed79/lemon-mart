bf2d3c905a62d4939148ad78f451d45cb0d8ab04
Bharath Seshadri <bharath.seshadri@photon.com>
2025-09-14 15:50:42 +0530

    Implemented routing for pos module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,5 +13,9 @@ export const routes: Routes = [
     path: 'user',
     loadChildren: () => import('./user/user-module').then((m) => m.UserModule),
   },
+  {
+    path: 'pos',
+    loadChildren: () => import('./pos/pos-module').then((m) => m.PosModule),
+  },
   { path: '*', component: PageNotFoundComponent },
 ];
---
+++ src/app/pos/pos-component/pos-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pos-component',
+  imports: [],
+  templateUrl: './pos-component.html',
+  styleUrl: './pos-component.scss'
+})
+export class PosComponent {
+
+}
